497b4e859f92bcb2557ba044d9ca4bb2fa68dc53
Michael Clark <michaelclarknashville@protonmail.com>
2020-03-13 18:04:41 -0500

    before

--- src/components/canvas/Canvas2.js
+++ src/components/canvas/Canvas2.js
@@ -1,23 +1,42 @@
 import React, { useEffect, useState } from "react";
 import API from "../../modules/ApiManager"
+import "./Canvas.css"
 
 const Canvas2 = (props) => {
 
-  const [classList, setClassList] = useState([])
+  const [classList, setClassList] = useState({id: "", name: ""})
 
-  const getClass = () => {
-    API.getWithId("className", 1)
+  const getClass = (str, id) => {
+    API.getWithId(str, id)
       .then(name => {
         setClassList(name)
       })
   }
 
+  const r = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min
+  }
+
+  function GFG_Fun() { 
+    var x = r(0,200); 
+    var y = r(); 
+    var el = document.getElementById('GFG_DIV'); 
+    el.style.position = "absolute"; 
+    el.style.left = x + 'px'; 
+    el.style.top = y + 'px'; 
+    el_down.innerHTML =  
+      "Position of element is changed."; 
+} 
+
+
   useEffect(() => {
-    getClass();
+    getClass("styles", 1);
   })
 
   return (
-    <div className={classList.class} ></div>
+    <div id="circle--1" className={classList.name} ></div>
   )
 }
 
